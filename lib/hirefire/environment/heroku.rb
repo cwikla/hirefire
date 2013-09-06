0270470d9c394591653a997c9a3ad6ec1e09dfab
cwikla <cwikla@thegiantpixel.com>
2013-09-06 13:03:29 -0700

    Fix some bugs. ick

--- lib/hirefire/environment/heroku.rb
+++ lib/hirefire/environment/heroku.rb
@@ -5,21 +5,43 @@ require 'heroku-api'
 module HireFire
   module Environment
     class Heroku < Base
+      TOO_OLD = ENV['HIREFIRE_TOO_OLD'] ? ENV['HIREFIRE_TOO_OLD'].to_i || 20
+
+      @@ps_cache = {}
 
       private
+
+      def too_old?(ts)
+        return Time.now - ts > TOO_OLD
+      end
+
+      def get_worker_count(app_name)
+        data = @@ps_cache(app_name)
+
+        return data[:worker_count] if !too_old?(data[:ts])
+
+        worker_count = client.get_ps(app_name).body.select {|p| p['process'] =~ /worker.[0-9]+/}.length
+        @@ps_cache[app_name] = { :worker_count => worker_count, :ts => Time.now }
+
+        return worker_count
+      end
+
+      def set_worker_count(app_name, amount)
+        val = client.post_ps_scale(app_name, "worker", amount)
+        @@ps_cache[app_name] = { :worker_count => amount, :ts => Time.now }
+        return val
+      end
 
       def workers(amount = nil)
 
         app_name = HireFire.configuration.app_name
         #puts "HIREFIRE FOR APP #{app_name}"
 
-        if amount.nil?
-          # return client.info(app_name)[:workers].to_i
-          return client.get_ps(app_name).body.select {|p| p['process'] =~ /worker.[0-9]+/}.length
-        end
+        return get_worker_count(app_name) if amount.nil?
 
         # client.set_workers(app_name], amount)
-        return client.post_ps_scale(app_name, "worker", amount)
+
+        return set_worker_count(app_name, amount)
 
       rescue Exception
         HireFire::Logger.message("Worker query request failed with #{ $!.class.name } #{ $!.message }")
